1e99fb1da01a6b3e7ecb867682a1daa564d511f3
qiang <guoshengqiang@live.com>
2019-03-15 16:10:09 +0800

    format

--- App.vue
+++ App.vue
@@ -1,112 +1,112 @@
 <script>
-    export default {
-        onLaunch: function() {
-            console.log('App Launch');
-        },
-        onShow: function() {
-            console.log('App Show');
-        },
-        onHide: function() {
-            console.log('App Hide');
-        }
-    }
+	export default {
+		onLaunch: function() {
+			console.log('App Launch');
+		},
+		onShow: function() {
+			console.log('App Show');
+		},
+		onHide: function() {
+			console.log('App Hide');
+		}
+	}
 </script>
 
 <style>
-    /*每个页面公共css */
-    /* uni-app默认全局使用flex布局。因为flex布局有利于跨更多平台，尤其是采用原生渲染的平台。如不了解flex布局，请参考http://www.w3.org/TR/css3-flexbox/。如不使用flex布局，请删除或注释掉本行。*/
-    body,
-    page {
-        min-height: 100%;
-        display: flex;
-    }
+	/*每个页面公共css */
+	/* uni-app默认全局使用flex布局。因为flex布局有利于跨更多平台，尤其是采用原生渲染的平台。如不了解flex布局，请参考http://www.w3.org/TR/css3-flexbox/。如不使用flex布局，请删除或注释掉本行。*/
+	body,
+	page {
+		min-height: 100%;
+		display: flex;
+	}
 
-    /* #ifdef MP-BAIDU */
-    page {
-        width: 100%;
-        height: 100%;
-        display: block;
-    }
+	/* #ifdef MP-BAIDU */
+	page {
+		width: 100%;
+		height: 100%;
+		display: block;
+	}
 
-    swan-template {
-        width: 100%;
-        min-height: 100%;
-        display: flex;
-    }
+	swan-template {
+		width: 100%;
+		min-height: 100%;
+		display: flex;
+	}
 
-    /* #endif */
+	/* #endif */
 
-    .content {
-        display: flex;
-        flex: 1;
-        flex-direction: column;
-        background-color: #efeff4;
-        padding: 20upx;
-    }
+	.content {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		background-color: #efeff4;
+		padding: 20upx;
+	}
 
-    .input-group {
-        background-color: #ffffff;
-        margin-top: 40upx;
-        position: relative;
-    }
+	.input-group {
+		background-color: #ffffff;
+		margin-top: 40upx;
+		position: relative;
+	}
 
-    .input-group::before {
-        position: absolute;
-        right: 0;
-        top: 0;
-        left: 0;
-        height: 1upx;
-        content: '';
-        -webkit-transform: scaleY(.5);
-        transform: scaleY(.5);
-        background-color: #c8c7cc;
-    }
+	.input-group::before {
+		position: absolute;
+		right: 0;
+		top: 0;
+		left: 0;
+		height: 1upx;
+		content: '';
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		background-color: #c8c7cc;
+	}
 
-    .input-group::after {
-        position: absolute;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        height: 1upx;
-        content: '';
-        -webkit-transform: scaleY(.5);
-        transform: scaleY(.5);
-        background-color: #c8c7cc;
-    }
+	.input-group::after {
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		height: 1upx;
+		content: '';
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		background-color: #c8c7cc;
+	}
 
-    .input-row {
-        display: flex;
-        flex-direction: row;
-        position: relative;
-    }
+	.input-row {
+		display: flex;
+		flex-direction: row;
+		position: relative;
+	}
 
-    .input-row .title {
-        width: 20%;
-        height: 50upx;
-        min-height: 50upx;
-        padding: 15upx 0;
-        padding-left: 30upx;
-        line-height: 50upx;
-    }
+	.input-row .title {
+		width: 20%;
+		height: 50upx;
+		min-height: 50upx;
+		padding: 15upx 0;
+		padding-left: 30upx;
+		line-height: 50upx;
+	}
 
-    .input-row.border::after {
-        position: absolute;
-        right: 0;
-        bottom: 0;
-        left: 15upx;
-        height: 1upx;
-        content: '';
-        -webkit-transform: scaleY(.5);
-        transform: scaleY(.5);
-        background-color: #c8c7cc;
-    }
+	.input-row.border::after {
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		left: 15upx;
+		height: 1upx;
+		content: '';
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		background-color: #c8c7cc;
+	}
 
-    .btn-row {
-        margin-top: 50upx;
-        padding: 20upx;
-    }
+	.btn-row {
+		margin-top: 50upx;
+		padding: 20upx;
+	}
 
-    button.primary {
-        background-color: #0faeff;
-    }
+	button.primary {
+		background-color: #0faeff;
+	}
 </style>
--- components/m-input.vue
+++ components/m-input.vue
@@ -1,128 +1,128 @@
 <template>
-    <view class="m-input-view">
-        <input :focus="focus_" :type="inputType" :value="value" @input="onInput" class="m-input-input" :placeholder="placeholder"
-            :password="type==='password'&&!showPassword" @focus="onFocus" @blur="onBlur" />
-        <!-- 优先显示密码可见按钮 -->
-        <view v-if="clearable_&&!displayable_&&value.length" class="m-input-icon">
-            <m-icon color="#666666" type="clear" size="20" @click="clear"></m-icon>
-        </view>
-        <view v-if="displayable_" class="m-input-icon">
-            <m-icon :color="showPassword?'#666666':'#cccccc'" type="eye" size="20" @click="display"></m-icon>
-        </view>
-    </view>
+	<view class="m-input-view">
+		<input :focus="focus_" :type="inputType" :value="value" @input="onInput" class="m-input-input" :placeholder="placeholder"
+		 :password="type==='password'&&!showPassword" @focus="onFocus" @blur="onBlur" />
+		<!-- 优先显示密码可见按钮 -->
+		<view v-if="clearable_&&!displayable_&&value.length" class="m-input-icon">
+			<m-icon color="#666666" type="clear" size="20" @click="clear"></m-icon>
+		</view>
+		<view v-if="displayable_" class="m-input-icon">
+			<m-icon :color="showPassword?'#666666':'#cccccc'" type="eye" size="20" @click="display"></m-icon>
+		</view>
+	</view>
 </template>
 
 <script>
-    import mIcon from './m-icon/m-icon.vue'
+	import mIcon from './m-icon/m-icon.vue'
 
-    export default {
-        components: {
-            mIcon
-        },
-        props: {
-            /**
-             * 输入类型
-             */
-            type: String,
-            /**
-             * 值
-             */
-            value: String,
-            /**
-             * 占位符
-             */
-            placeholder: String,
-            /**
-             * 是否显示清除按钮
-             */
-            clearable: {
-                type: [Boolean, String],
-                default: false
-            },
-            /**
-             * 是否显示密码可见按钮
-             */
-            displayable: {
-                type: [Boolean, String],
-                default: false
-            },
-            /**
-             * 自动获取焦点
-             */
-            focus: {
-                type: [Boolean, String],
-                default: false
-            }
-        },
-        model: {
-            prop: 'value',
-            event: 'input'
-        },
-        data() {
-            return {
-                /**
-                 * 显示密码明文
-                 */
-                showPassword: false,
-                /**
-                 * 是否获取焦点
-                 */
-                isFocus: false
-            }
-        },
-        computed: {
-            inputType() {
-                const type = this.type
-                return type === 'password' ? 'text' : type
-            },
-            clearable_() {
-                return String(this.clearable) !== 'false'
-            },
-            displayable_() {
-                return String(this.displayable) !== 'false'
-            },
-            focus_() {
-                return String(this.focus) !== 'false'
-            }
-        },
-        methods: {
-            clear() {
-                this.value = ''
-            },
-            display() {
-                this.showPassword = !this.showPassword
-            },
-            onFocus() {
-                this.isFocus = true
-            },
-            onBlur() {
-                this.$nextTick(() => {
-                    this.isFocus = false
-                })
-            },
-            onInput(e) {
-                this.$emit('input', e.target.value)
-            }
-        }
-    }
+	export default {
+		components: {
+			mIcon
+		},
+		props: {
+			/**
+			 * 输入类型
+			 */
+			type: String,
+			/**
+			 * 值
+			 */
+			value: String,
+			/**
+			 * 占位符
+			 */
+			placeholder: String,
+			/**
+			 * 是否显示清除按钮
+			 */
+			clearable: {
+				type: [Boolean, String],
+				default: false
+			},
+			/**
+			 * 是否显示密码可见按钮
+			 */
+			displayable: {
+				type: [Boolean, String],
+				default: false
+			},
+			/**
+			 * 自动获取焦点
+			 */
+			focus: {
+				type: [Boolean, String],
+				default: false
+			}
+		},
+		model: {
+			prop: 'value',
+			event: 'input'
+		},
+		data() {
+			return {
+				/**
+				 * 显示密码明文
+				 */
+				showPassword: false,
+				/**
+				 * 是否获取焦点
+				 */
+				isFocus: false
+			}
+		},
+		computed: {
+			inputType() {
+				const type = this.type
+				return type === 'password' ? 'text' : type
+			},
+			clearable_() {
+				return String(this.clearable) !== 'false'
+			},
+			displayable_() {
+				return String(this.displayable) !== 'false'
+			},
+			focus_() {
+				return String(this.focus) !== 'false'
+			}
+		},
+		methods: {
+			clear() {
+				this.value = ''
+			},
+			display() {
+				this.showPassword = !this.showPassword
+			},
+			onFocus() {
+				this.isFocus = true
+			},
+			onBlur() {
+				this.$nextTick(() => {
+					this.isFocus = false
+				})
+			},
+			onInput(e) {
+				this.$emit('input', e.target.value)
+			}
+		}
+	}
 </script>
 
 <style>
-    .m-input-view {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        width: 100%;
-        flex: 1;
-        padding: 0 10rpx;
-    }
+	.m-input-view {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		width: 100%;
+		flex: 1;
+		padding: 0 10rpx;
+	}
 
-    .m-input-input {
-        flex: 1;
-        width: 100%;
-    }
-    
-    .m-input-icon{
-        width: 20px;
-    }
+	.m-input-input {
+		flex: 1;
+		width: 100%;
+	}
+
+	.m-input-icon {
+		width: 20px;
+	}
 </style>
