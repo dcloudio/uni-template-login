88d8adc3e41c52d912724d7b670de7c74b6f4663
qiang <guoshengqiang@live.com>
2019-07-05 15:41:20 +0800

    解决支付宝平台页面高度100%未生效的问题

--- App.vue
+++ App.vue
@@ -14,8 +14,6 @@
 
 <style>
 	/*每个页面公共css */
-	/* uni-app默认全局使用flex布局。因为flex布局有利于跨更多平台，尤其是采用原生渲染的平台。如不了解flex布局，请参考http://www.w3.org/TR/css3-flexbox/。如不使用flex布局，请删除或注释掉本行。*/
-	body,
 	page {
 		min-height: 100%;
 		display: flex;
@@ -39,6 +37,13 @@
 		width: 100%;
 		min-height: 100%;
 		display: flex;
+	}
+
+	/* #endif */
+
+	/* #ifdef MP-ALIPAY */
+	page {
+		min-height: 100vh;
 	}
 
 	/* #endif */
