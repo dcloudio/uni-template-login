861222ef3316c6367cedf797f40ad2ddd9d7d541
qiang <guoshengqiang@live.com>
2019-12-17 20:30:00 +0800

    format

--- service.js
+++ service.js
@@ -2,25 +2,25 @@
 const USERS_KEY = 'USERS_KEY';
 const STATE_KEY = 'STATE_KEY';
 
-const getUsers = function () {
-    let ret = '';
-    ret = uni.getStorageSync(USERS_KEY);
-    if (!ret) {
-        ret = '[]';
-    }
-    return JSON.parse(ret);
+const getUsers = function() {
+	let ret = '';
+	ret = uni.getStorageSync(USERS_KEY);
+	if (!ret) {
+		ret = '[]';
+	}
+	return JSON.parse(ret);
 }
 
-const addUser = function (userInfo) {
-    let users = getUsers();
-    users.push({
-        account: userInfo.account,
-        password: userInfo.password
-    });
-    uni.setStorageSync(USERS_KEY, JSON.stringify(users));
+const addUser = function(userInfo) {
+	let users = getUsers();
+	users.push({
+		account: userInfo.account,
+		password: userInfo.password
+	});
+	uni.setStorageSync(USERS_KEY, JSON.stringify(users));
 }
 
 export default {
-    getUsers,
-    addUser
+	getUsers,
+	addUser
 }
--- store/index.js
+++ store/index.js
@@ -4,24 +4,24 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-    state: {
-        /**
-         * 是否需要强制登录
-         */
-        forcedLogin: false,
-        hasLogin: false,
-        userName: ""
-    },
-    mutations: {
-        login(state, userName) {
-            state.userName = userName || '新用户';
-            state.hasLogin = true;
-        },
-        logout(state) {
-            state.userName = "";
-            state.hasLogin = false;
-        }
-    }
+	state: {
+		/**
+		 * 是否需要强制登录
+		 */
+		forcedLogin: false,
+		hasLogin: false,
+		userName: ""
+	},
+	mutations: {
+		login(state, userName) {
+			state.userName = userName || '新用户';
+			state.hasLogin = true;
+		},
+		logout(state) {
+			state.userName = "";
+			state.hasLogin = false;
+		}
+	}
 })
 
 export default store
