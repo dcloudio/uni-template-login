ef889b06f17443c4f1a1246120aa2526db91afcb
hbcui1984 <hbcui1984@163.com>
2020-06-23 21:29:45 +0800

    添加user-center云函数

---
+++ cloudfunctions-aliyun/user-center/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const uniID = require('uni-id')
+
+exports.main = async (event, context) => {
+    //event为客户端上传的参数
+    console.log('event : ' + event)
+
+
+    let params = event.params
+    let res = {}
+
+    let payload = {}
+
+    switch (event.action) {
+        case 'register':
+            res = uniID.register(event.params, context);
+            break;
+        case 'login':
+            res = uniID.login(event.params, context);
+            break;
+        case 'checkToken':
+            res = uniID.checkToken(event.uniIdToken);
+            break;
+        case 'logout':
+            res = await uniID.logout(event.uniIdToken)
+            break;
+        default:
+            res = {
+                code: 403,
+                msg: '非法访问'
+            }
+            break;
+    }
+
+    //返回数据给客户端
+    return res
+};
