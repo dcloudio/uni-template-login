b976cd7f72db8db0cb85d95bfbf970143c347451
tianjiaxing <tianjiaxing2016@163.com>
2020-07-28 21:59:51 +0800

    add: 完善个人中心

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,8 +1,47 @@
 <template>
-	<view class="content">
-		<view class="btn-row">
-			<button v-if="!hasLogin" type="primary" class="primary" @tap="bindLogin">登录</button>
-			<button v-if="hasLogin" type="default" @tap="bindLogout">退出登录</button>
+	<view class="">
+		<view class="center">
+			<view class="logo" @click="bindLogin" :hover-class="!hasLogin ? 'logo-hover' : ''">
+				<image class="logo-img" :src="avatarUrl"></image>
+				<view class="logo-title">
+					<text class="uer-name">Hi，{{hasLogin ? userName : '您未登录'}}</text>
+					<text class="go-login navigat-arrow" v-if="!hasLogin">&#xe65e;</text>
+				</view>
+			</view>
+			<view class="center-list">
+				<view class="center-list-item border-bottom">
+					<text class="list-icon">&#xe60f;</text>
+					<text class="list-text">帐号管理</text>
+					<text class="navigat-arrow">&#xe65e;</text>
+				</view>
+				<view class="center-list-item">
+					<text class="list-icon">&#xe639;</text>
+					<text class="list-text">新消息通知</text>
+					<text class="navigat-arrow">&#xe65e;</text>
+				</view>
+			</view>
+			<view class="center-list">
+				<view class="center-list-item border-bottom">
+					<text class="list-icon">&#xe60b;</text>
+					<text class="list-text">帮助与反馈</text>
+					<text class="navigat-arrow">&#xe65e;</text>
+				</view>
+				<view class="center-list-item">
+					<text class="list-icon">&#xe65f;</text>
+					<text class="list-text">服务条款及隐私</text>
+					<text class="navigat-arrow">&#xe65e;</text>
+				</view>
+			</view>
+			<view class="center-list">
+				<view class="center-list-item">
+					<text class="list-icon">&#xe614;</text>
+					<text class="list-text">关于应用</text>
+					<text class="navigat-arrow">&#xe65e;</text>
+				</view>
+			</view>
+			<view class="btn-row">
+				<button v-if="hasLogin" type="default" @tap="bindLogout">退出登录</button>
+			</view>
 		</view>
 	</view>
 </template>
@@ -14,8 +53,13 @@
 	} from 'vuex'
 
 	export default {
+		data() {
+			return {
+				avatarUrl: "../../static/img/logo.png",
+			}
+		},
 		computed: {
-			...mapState(['hasLogin', 'forcedLogin'])
+			...mapState(['hasLogin', 'forcedLogin', 'userName'])
 		},
 		methods: {
 			...mapMutations(['logout']),
@@ -79,5 +123,127 @@
 </script>
 
 <style>
+	@font-face {
+		font-family: texticons;
+		font-weight: normal;
+		font-style: normal;
+		src: url('https://at.alicdn.com/t/font_984210_5cs13ndgqsn.ttf') format('truetype');
+	}
 
+	page,
+	view {
+		display: flex;
+	}
+
+	page {
+		background-color: #f8f8f8;
+	}
+	
+	button {
+		width: 100%;
+	}
+
+	.center {
+		flex-direction: column;
+	}
+
+	.logo {
+		width: 750upx;
+		height: 240upx;
+		padding: 20upx;
+		box-sizing: border-box;
+		background-color: #0faeff;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	.logo-hover {
+		opacity: 0.8;
+	}
+
+	.logo-img {
+		width: 150upx;
+		height: 150upx;
+		border-radius: 150upx;
+	}
+
+	.logo-title {
+		height: 150upx;
+		flex: 1;
+		align-items: center;
+		justify-content: space-between;
+		flex-direction: row;
+		margin-left: 20upx;
+	}
+
+	.uer-name {
+		height: 60upx;
+		line-height: 60upx;
+		font-size: 38upx;
+		color: #FFFFFF;
+	}
+
+	.go-login.navigat-arrow {
+		font-size: 38upx;
+		color: #FFFFFF;
+	}
+
+	.login-title {
+		height: 150upx;
+		align-items: self-start;
+		justify-content: center;
+		flex-direction: column;
+		margin-left: 20upx;
+	}
+
+	.center-list {
+		background-color: #FFFFFF;
+		margin-top: 20upx;
+		width: 750upx;
+		flex-direction: column;
+	}
+
+	.center-list-item {
+		height: 90upx;
+		width: 750upx;
+		box-sizing: border-box;
+		flex-direction: row;
+		padding: 0upx 20upx;
+	}
+
+	.border-bottom {
+		border-bottom-width: 1upx;
+		border-color: #c8c7cc;
+		border-bottom-style: solid;
+	}
+
+	.list-icon {
+		width: 40upx;
+		height: 90upx;
+		line-height: 90upx;
+		font-size: 34upx;
+		color: #0faeff;
+		text-align: center;
+		font-family: texticons;
+		margin-right: 20upx;
+	}
+
+	.list-text {
+		height: 90upx;
+		line-height: 90upx;
+		font-size: 34upx;
+		color: #555;
+		flex: 1;
+		text-align: left;
+	}
+
+	.navigat-arrow {
+		height: 90upx;
+		width: 40upx;
+		line-height: 90upx;
+		font-size: 34upx;
+		color: #555;
+		text-align: right;
+		font-family: texticons;
+	}
 </style>
